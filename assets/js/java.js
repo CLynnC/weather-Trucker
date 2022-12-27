c6e532b9a7e451bb882435bdd05b350011f48095
CLynnC <crystal.lynnmiller@mail.com>
2022-12-26 22:51:51 -0600

    Got pretty darn far in populating the current data to current chosen city on search. However, I hit a snag on the icon. So am saving what I have until I can gain some insight on that. ;-(

--- assets/js/java.js
+++ assets/js/java.js
@@ -2,15 +2,15 @@ var userInput = document.querySelector("#search-bar");
 
 var currentDate = document.querySelector(".today");
 
-var APIKey = "77424e25aab1562d1c6c462752230d46";
-
+//var APIKey = "77424e25aab1562d1c6c462752230d46";
+var APIKey = "323f1dc6bac5ba0a2f91ab4d1f418500";
 
 document.getElementById("search").addEventListener('click', searchIng);
 
 var currentDate = dayjs();
 $('.today').text(dayjs().format('M/DD/YYYY'));
 
-
+//var img = document.querySelector(".condition",);
 
 
 
@@ -29,7 +29,7 @@ function searchIng(event) {
     let userInput = 
     document.getElementById("search-bar").value;
     console.log(userInput);
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey + "&units=imperial";
     let forcastURL=`http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=${APIKey}`
     fetch(queryURL)
         .then(response => response.json())
@@ -37,13 +37,33 @@ function searchIng(event) {
             let cityName = (data.name)
             var cityContainer = document.querySelector(".cityName");
             cityContainer.innerHTML = cityName, "";
+            
             let currentIcon = (data.weather[0].icon)
-            let image = document.createElement("img");
-                image.src = "http://openweathermap.org/img/wn/" + currentIcon + ".png";
-                image.width = 100;
-                image.height = 110;
+            
+            var img = document.createElement("img");
+                img.src = ("http://openweathermap.org/img/wn/" + currentIcon + ".png");
+            var block = document.getElementById("condition");
+                block.appendChild(img);
+                        
+                    
+                //if (block === img) {
+                     //  block.innerHTML = new (img)}
+               //}}  else {
+            //        block.replaceChild(img , img);
+               // }
+            
+               
+            //let img = document.createElement("img");
+            //img.src = `http://openweathermap.org/img/wn/${currentIcon}.png`;
+                //img.width = 100
+                //img.height = 110  
+            //var imageContainer = document.querySelector(".condition");
+            //imageContainer.innerHTML.append(img);
+            console.log(img);
 
-$('.condition').append(image);
+            let currentTemp = (data.main.temp)
+            var tempContainer = document.querySelector(".temp");
+                tempContainer.innerHTML = "Temp:   _ " + currentTemp;
             //document.body.
             //iconContainer.innerHTML = currentIcon;
             console.log(cityName)
@@ -51,9 +71,7 @@ $('.condition').append(image);
 
     
            // console.log(data.main.temp)
-        });
+        })
 }
 
-
-alert(userInput)
-console.log(userInput)
+//document.getElementById('search').onclick = function searchIng(){}
