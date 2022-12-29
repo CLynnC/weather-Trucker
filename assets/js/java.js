c537d7f3ac3d2ef34b3b7caf44961bab40a89539
CLynnC <crystal.lynnmiller@mail.com>
2022-12-29 15:27:01 -0600

    Finished the five-day. Had to figure out how to get the dates right and the icons, but finally finished 'er.

--- assets/js/java.js
+++ assets/js/java.js
@@ -33,7 +33,7 @@ function searchIng() {
             let currentIcon = (data.weather[0].icon)   
             window.image = document.createElement('img');
                     image.src =("http://openweathermap.org/img/wn/" + currentIcon +".png");
-                    image.width = 80
+                    image.widt_txth = 80
                     image.height = 90   
             let imageContainer = $('#current').children().eq(2);
             imageContainer.replaceWith(image);
@@ -54,78 +54,120 @@ function searchIng() {
     fetch(forcastURL)
         .then(response => response.json())
         .then(data => {
-            let date1 = (data.list[1].dt)
+            let date1 = (data.list[5].dt_txt)
             $('#date1').text(dayjs(date1).format('M/DD/YYYY'));
+
+            let forcast1Icon = (data.list[5].weather[0].icon)   
+            window.image1 = document.createElement('img');
+                    image1.src =("http://openweathermap.org/img/wn/" + forcast1Icon +".png");
+                    image1.widt_txth = 80
+                    image1.height = 90   
+            let image1Container = $('#forecast').children().eq(0).children().eq(1);
+            image1Container.replaceWith(image1);
             
-            let temp1 = (data.list[1].main.temp)
+            let temp1 = (data.list[5].main.temp)
             var containerOne = document.querySelector("#temp1");
                 containerOne.innerHTML = "Temp: _ " + temp1 + "\u00B0" ;
 
-            let wind1 = (data.list[1].wind.speed)
+            let wind1 = (data.list[5].wind.speed)
             var windContainerOne = document.querySelector("#wind1");
                 windContainerOne.innerHTML = "Wind: _ " + wind1 + " MPH";
 
-            let humidity1 = (data.list[1].main.humidity)
+            let humidity1 = (data.list[5].main.humidity)
             var humidityContainerOne = document.querySelector("#humidity1");
                 humidityContainerOne.innerHTML = "Humidity: _ " + humidity1 + " %";
 
-                let date2 = (data.list[2].dt)
+                let date2 = (data.list[13].dt_txt)
                 $('#date2').text(dayjs(date2).format('M/DD/YYYY'));
+
+                let forcast2Icon = (data.list[13].weather[0].icon)   
+            window.image2 = document.createElement('img');
+                    image2.src =("http://openweathermap.org/img/wn/" + forcast1Icon +".png");
+                    image2.widt_txth = 80
+                    image2.height = 90   
+            let image2Container = document.querySelector("#icon2");
+            image2Container.replaceWith(image2);
                 
-                let temp2 = (data.list[2].main.temp)
+                let temp2 = (data.list[13].main.temp)
                 var containerTwo = document.querySelector("#temp2");
                     containerTwo.innerHTML = "Temp: _ " + temp2 + "\u00B0" ;
     
-                let wind2 = (data.list[2].wind.speed)
+                let wind2 = (data.list[13].wind.speed)
                 var windContainerTwo = document.querySelector("#wind2");
                     windContainerTwo.innerHTML = "Wind: _ " + wind2 + " MPH";
     
-                let humidity2 = (data.list[2].main.humidity)
+                let humidity2 = (data.list[13].main.humidity)
                 var humidityContainerTwo = document.querySelector("#humidity2");
                     humidityContainerTwo.innerHTML = "Humidity: _ " + humidity2 + " %";
 
-                    let date3 = (data.list[3].dt)
+                    let date3 = (data.list[21].dt_txt)
             $('#date3').text(dayjs(date3).format('M/DD/YYYY'));
             
-            let temp3 = (data.list[3].main.temp)
+            let forcast3Icon = (data.list[21].weather[0].icon)   
+            window.image3 = document.createElement('img');
+                    image3.src =("http://openweathermap.org/img/wn/" + forcast3Icon +".png");
+                    image3.widt_txth = 80
+                    image3.height = 90   
+            let image3Container = document.querySelector("#icon3");
+            image3Container.replaceWith(image3);
+
+            let temp3 = (data.list[21].main.temp)
             var containerThree = document.querySelector("#temp3");
                 containerThree.innerHTML = "Temp: _ " + temp3 + "\u00B0" ;
 
-            let wind3 = (data.list[3].wind.speed)
+            let wind3 = (data.list[21].wind.speed)
             var windContainerThree = document.querySelector("#wind3");
                 windContainerThree.innerHTML = "Wind: _ " + wind3 + " MPH";
 
-            let humidity3 = (data.list[3].main.humidity)
+            let humidity3 = (data.list[21].main.humidity)
             var humidityContainerThree = document.querySelector("#humidity3");
                 humidityContainerThree.innerHTML = "Humidity: _ " + humidity3 + " %";
 
-                let date4 = (data.list[4].dt)
+                let date4 = (data.list[29].dt_txt)
             $('#date4').text(dayjs(date4).format('M/DD/YYYY'));
             
-            let temp4 = (data.list[4].main.temp)
+            let forcast4Icon = (data.list[29].weather[0].icon)   
+            window.image4 = document.createElement('img');
+                    image4.src =("http://openweathermap.org/img/wn/" + forcast4Icon + ".png");
+                    image4.widt_txth = 80
+                    image4.height = 90   
+            let image4Container = document.querySelector('#icon4');
+            image4Container.replaceWith(image4);
+
+            console.log(window.image4);
+
+            let temp4 = (data.list[29].main.temp)
             var containerFour = document.querySelector("#temp4");
                 containerFour.innerHTML = "Temp: _ " + temp4 + "\u00B0" ;
 
-            let wind4 = (data.list[4].wind.speed)
+            let wind4 = (data.list[29].wind.speed)
             var windContainerFour = document.querySelector("#wind4");
                 windContainerFour.innerHTML = "Wind: _ " + wind4 + " MPH";
 
-            let humidity4 = (data.list[4].main.humidity)
+            let humidity4 = (data.list[29].main.humidity)
             var humidityContainerFour = document.querySelector("#humidity4");
                 humidityContainerFour.innerHTML = "Humidity: _ " + humidity4 + " %";
 
-                let date5 = (data.list[5].dt)
+                let date5 = (data.list[37].dt_txt)
             $('#date5').text(dayjs(date5).format('M/DD/YYYY'));
             
-            let temp5 = (data.list[5].main.temp)
+            let forcast5Icon = (data.list[37].weather[0].icon)   
+            window.image5 = document.createElement('img');
+                    image5.src =("http://openweathermap.org/img/wn/" + forcast5Icon +".png");
+                    image5.widt_txth = 80
+                    image5.height = 90   
+            let image5Container = document.querySelector("#icon5");
+            image5Container.replaceWith(image5);
+
+            let temp5 = (data.list[37].main.temp)
             var containerFive = document.querySelector("#temp5");
                 containerFive.innerHTML = "Temp: _ " + temp5 + "\u00B0" ;
 
-            let wind5 = (data.list[5].wind.speed)
+            let wind5 = (data.list[37].wind.speed)
             var windContainerFive = document.querySelector("#wind5");
                 windContainerFive.innerHTML = "Wind: _ " + wind5 + " MPH";
 
-            let humidity5 = (data.list[5].main.humidity)
+            let humidity5 = (data.list[37].main.humidity)
             var humidityContainerFive = document.querySelector("#humidity5");
                 humidityContainerFive.innerHTML = "Humidity: _ " + humidity5 + " %";
             
