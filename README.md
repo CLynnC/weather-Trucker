77da8d1e1d9a2204ac17c6bafdc894da5d19edde
CLynnC <crystal.lynnmiller@mail.com>
2023-01-06 18:45:19 -0600

    Finally finished this project. And am saving to add a screenshot.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # weather-Trucker
 The following is the repo-generated for the Challenge#6 Assignment.
+
+# <Project-Title: Weather Trucker>
+## Description
+       For our Week6 Challenge Assignment, we were prompted to create a Weather Dashboard. That incorporated Local Storage along with some other elements of using web APIs in a sleek and polished user interface web application. 
+## Usage & ScreenShot >>
+    I believe the original readme acceptance criterium, explains the usage well enough...
+
+    GIVEN a weather dashboard with form inputs
+        WHEN I search for a city
+    ✅ THEN I am presented with current and future conditions for that city and that city is added to the search history
+        WHEN I view current weather conditions for that city
+    ✅ THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+        WHEN I view future weather conditions for that city
+    ✅ THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+        WHEN I click on a city in the search history
+    ✅ THEN I am again presented with current and future conditions for that city
+
+
+![ScreenShot1](https://{url})
+
+![ScreenShot2](https://{url})
+
+## Features
+    My Weather Dashboard contains a sleek and polished UI and UX look. It includes a searchbar with initial prompt text to search for a city...
+    It also includes a search button and a list to the side to display all previously searched cities.
+
+    Note: I had to acquire the aid of a tutor in order to figure out the local storage out and set it up so that it would start the user out with input from local storage if there was any. And also so that it would be accessable to the user add this input to the page upon load.
+
+    It includes a main dashboard box with the city name in a heading format for easy viewing. The current tempature and an icon to demonstrate current weather conditions.
+    Along with this it displays the date between the city and the weather icon.
+    It includes a small section below that with the current temp, windspeed, and humidity.
+    Below this are 5 smaller boxes to display the same city's five-day forecast, as predicted through the data obtained via the web API used.
+
+    For this project, we used the OpenWeatherAPI web api. https://openweathermap.org/
+
+    It also features dynamic Javascripting with inputs to localStorage for city and weather details.
+
+## How to Contribute
+
+I take Paypal  :,-( 
+    https://www.paypal.com/donate/?hosted_button_id=3HEM2ALNGHC42
+
--- assets/js/java.js
+++ assets/js/java.js
@@ -1,188 +1,221 @@
-var userInput = document.querySelector("#search-bar");
 
-var currentDate = document.querySelector(".today");
+var userInput = document.querySelector('#search-bar')
 
-//var APIKey = "77424e25aab1562d1c6c462752230d46";
-var APIKey = "323f1dc6bac5ba0a2f91ab4d1f418500";
+var currentDate = document.querySelector('.today')
 
-document.getElementById("search").addEventListener('click', searchIng);
+var cityList = document.querySelector('#saved-cities')
 
-var currentDate = dayjs();
-$('.today').text(dayjs().format('M/DD/YYYY'));
+// var APIKey = "77424e25aab1562d1c6c462752230d46"   <---- Had to use a second APIKey because this one expired while I was trying to work on this assignment. I Spend WAaaaaaaay 2 much time on this one! More time than I had. And I was trying after we left for the holidays to just do the minimum. -_-')
+var APIKey = '323f1dc6bac5ba0a2f91ab4d1f418500'
 
-var userInput;
+document.getElementById('search').addEventListener('click', postMe)
 
-
-
+var currentDate = dayjs()
+$('.today').text(dayjs().format('M/DD/YYYY'))
 
 
 
+function postMe () {
+    var cityName = document.getElementById('search-bar').value
+    searchIng(cityName)
+}
 
-function searchIng(event) {
-    let userInput = 
-    document.getElementById("search-bar").value;
-    console.log(userInput);
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey + "&units=imperial";
-    
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            let cityName = (data.name)
-            var cityContainer = document.querySelector(".cityName");
-            cityContainer.innerHTML = cityName, "";
-            
-            let currentIcon = (data.weather[0].icon)   
-            window.image = document.createElement('img');
-                    image.src =("http://openweathermap.org/img/wn/" + currentIcon +".png");
-                    image.widt_txth = 80
-                    image.height = 90   
-            let imageContainer = $('#current').children().eq(2);
-            imageContainer.replaceWith(image);
-            console.log(window.image);
 
-            let currentTemp = (data.main.temp)
-            var tempContainer = document.querySelector(".temp");
-                tempContainer.innerHTML = "Temp:   _ " + currentTemp;
-            
-            let currentWind = (data.wind.speed)
-            var windSpeedContainer = document.querySelector(".wind");
-                windSpeedContainer.innerHTML = "Wind Speed:  __ " + currentWind + " MPH";
-
-            let currentHumidity = (data.main.humidity)
-            var currentHumidityContainer = document.querySelector(".humidity");
-                currentHumidityContainer.innerHTML = "Humidity:  _ " + currentHumidity + " %";
-        })
-        
-        var forcastURL= "http://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + APIKey + "&units=imperial";
-    fetch(forcastURL)
-        .then(response => response.json())
-        .then(data => {
-            let date1 = (data.list[5].dt_txt)
-            $('#date1').text(dayjs(date1).format('M/DD/YYYY'));
-
-            let forcast1Icon = (data.list[5].weather[0].icon)   
-            window.image1 = document.createElement('img');
-                    image1.src =("http://openweathermap.org/img/wn/" + forcast1Icon +".png");
-                    image1.widt_txth = 80
-                    image1.height = 90   
-            let image1Container = $('#forecast').children().eq(0).children().eq(1);
-            image1Container.replaceWith(image1);
-            
-            let temp1 = (data.list[5].main.temp)
-            var containerOne = document.querySelector("#temp1");
-                containerOne.innerHTML = "Temp: _ " + temp1 + "\u00B0" ;
-
-            let wind1 = (data.list[5].wind.speed)
-            var windContainerOne = document.querySelector("#wind1");
-                windContainerOne.innerHTML = "Wind: _ " + wind1 + " MPH";
-
-            let humidity1 = (data.list[5].main.humidity)
-            var humidityContainerOne = document.querySelector("#humidity1");
-                humidityContainerOne.innerHTML = "Humidity: _ " + humidity1 + " %";
-
-                let date2 = (data.list[13].dt_txt)
-                $('#date2').text(dayjs(date2).format('M/DD/YYYY'));
-
-                let forcast2Icon = (data.list[13].weather[0].icon)   
-            window.image2 = document.createElement('img');
-                    image2.src =("http://openweathermap.org/img/wn/" + forcast2Icon +".png");
-                    image2.widt_txth = 80
-                    image2.height = 90   
-            let image2Container = $('#forecast').children().eq(2).children().eq(1);
-            image2Container.replaceWith(image2);
-
-            console.log(image2);
-                
-                let temp2 = (data.list[13].main.temp)
-                var containerTwo = document.querySelector("#temp2");
-                    containerTwo.innerHTML = "Temp: _ " + temp2 + "\u00B0" ;
-    
-                let wind2 = (data.list[13].wind.speed)
-                var windContainerTwo = document.querySelector("#wind2");
-                    windContainerTwo.innerHTML = "Wind: _ " + wind2 + " MPH";
-    
-                let humidity2 = (data.list[13].main.humidity)
-                var humidityContainerTwo = document.querySelector("#humidity2");
-                    humidityContainerTwo.innerHTML = "Humidity: _ " + humidity2 + " %";
-
-                    let date3 = (data.list[21].dt_txt)
-            $('#date3').text(dayjs(date3).format('M/DD/YYYY'));
-            
-            let forcast3Icon = (data.list[21].weather[0].icon)   
-            window.image3 = document.createElement('img');
-                    image3.src =("http://openweathermap.org/img/wn/" + forcast3Icon +".png");
-                    image3.widt_txth = 80
-                    image3.height = 90   
-            let image3Container = $("#forecast").children().eq(4).children().eq(1);
-            image3Container.replaceWith(image3);
-
-            let temp3 = (data.list[21].main.temp)
-            var containerThree = document.querySelector("#temp3");
-                containerThree.innerHTML = "Temp: _ " + temp3 + "\u00B0" ;
-
-            let wind3 = (data.list[21].wind.speed)
-            var windContainerThree = document.querySelector("#wind3");
-                windContainerThree.innerHTML = "Wind: _ " + wind3 + " MPH";
-
-            let humidity3 = (data.list[21].main.humidity)
-            var humidityContainerThree = document.querySelector("#humidity3");
-                humidityContainerThree.innerHTML = "Humidity: _ " + humidity3 + " %";
-
-                let date4 = (data.list[29].dt_txt)
-            $('#date4').text(dayjs(date4).format('M/DD/YYYY'));
-            
-            let forcast4Icon = (data.list[29].weather[0].icon)   
-            window.image4 = document.createElement('img');
-                    image4.src =("http://openweathermap.org/img/wn/" + forcast4Icon + ".png");
-                    image4.widt_txth = 80
-                    image4.height = 90   
-            let image4Container = $('#forecast').children().eq(6).children().eq(1);
-            image4Container.replaceWith(image4);
-
-            console.log(window.image4);
-
-            let temp4 = (data.list[29].main.temp)
-            var containerFour = document.querySelector("#temp4");
-                containerFour.innerHTML = "Temp: _ " + temp4 + "\u00B0" ;
-
-            let wind4 = (data.list[29].wind.speed)
-            var windContainerFour = document.querySelector("#wind4");
-                windContainerFour.innerHTML = "Wind: _ " + wind4 + " MPH";
-
-            let humidity4 = (data.list[29].main.humidity)
-            var humidityContainerFour = document.querySelector("#humidity4");
-                humidityContainerFour.innerHTML = "Humidity: _ " + humidity4 + " %";
-
-                let date5 = (data.list[37].dt_txt)
-            $('#date5').text(dayjs(date5).format('M/DD/YYYY'));
-            
-            let forcast5Icon = (data.list[37].weather[0].icon)   
-            window.image5 = document.createElement('img');
-                    image5.src =("http://openweathermap.org/img/wn/" + forcast5Icon +".png");
-                    image5.widt_txth = 80
-                    image5.height = 90   
-            let image5Container = $("#forecast").children().eq(8).children().eq(1);
-            image5Container.replaceWith(image5);
-
-            let temp5 = (data.list[37].main.temp)
-            var containerFive = document.querySelector("#temp5");
-                containerFive.innerHTML = "Temp: _ " + temp5 + "\u00B0" ;
-
-            let wind5 = (data.list[37].wind.speed)
-            var windContainerFive = document.querySelector("#wind5");
-                windContainerFive.innerHTML = "Wind: _ " + wind5 + " MPH";
-
-            let humidity5 = (data.list[37].main.humidity)
-            var humidityContainerFive = document.querySelector("#humidity5");
-                humidityContainerFive.innerHTML = "Humidity: _ " + humidity5 + " %";
-            
-        localStorage.setItem("cityName", userInput);
-                //document.body.
-            //iconContainer.innerHTML = currentIcon;
-            
-            console.log(data)
-
-    
-           // console.log(data.main.temp)
-        })
+function saveToStorage (newCity) {
+    console.log("saving to storage")
+    console.log(newCity)
+    var history = JSON.parse(localStorage.getItem('cityList'))
+    console.log(history)
+    if(history.includes(newCity)){
+        return
     }
+    history.push(newCity)
+    if(history.length > 5){
+        history.shift()
+    }
+    localStorage.setItem('cityList', JSON.stringify(history))
+
+}
+
+function storage (event) {
+    event.preventDefault()
+    console.log(event)
+    console.log(event.target)
+    console.log(this)
+    searchIng(event.target.innerHTML)
+}
+
+function searchIng (searchCity) {
+  var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&appid=' + APIKey + '&units=imperial'
+
+  fetch(queryURL)
+    .then(response => response.json())
+    .then(data => {
+      let cityName = (data.name)
+      saveToStorage(cityName)
+      var cityContainer = document.querySelector('.cityName')
+      cityContainer.innerHTML = cityName, ''
+
+      let currentIcon = (data.weather[0].icon)
+      window.image = document.createElement('img')
+      image.src = ('http://openweathermap.org/img/wn/' + currentIcon + '.png')
+      image.widt_txth = 80
+      image.height = 90
+      let imageContainer = $('#current').children().eq(2)
+      imageContainer.replaceWith(image)
+      console.log(window.image)
+
+      let currentTemp = (data.main.temp)
+      var tempContainer = document.querySelector('.temp')
+      tempContainer.innerHTML = 'Temp:   _ ' + currentTemp
+
+      let currentWind = (data.wind.speed)
+      var windSpeedContainer = document.querySelector('.wind')
+      windSpeedContainer.innerHTML = 'Wind Speed:  __ ' + currentWind + ' MPH'
+
+      let currentHumidity = (data.main.humidity)
+      var currentHumidityContainer = document.querySelector('.humidity')
+      currentHumidityContainer.innerHTML = 'Humidity:  _ ' + currentHumidity + ' %'
+    })
+
+  var forcastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchCity + '&appid=' + APIKey + '&units=imperial'
+  fetch(forcastURL)
+    .then(response => response.json())
+    .then(data => {
+      let date1 = (data.list[5].dt_txt)
+      $('#date1').text(dayjs(date1).format('M/DD/YYYY'))
+
+      let forcast1Icon = (data.list[5].weather[0].icon)
+      window.image1 = document.createElement('img')
+      image1.src = ('http://openweathermap.org/img/wn/' + forcast1Icon + '.png')
+      image1.widt_txth = 80
+      image1.height = 90
+      let image1Container = $('#forecast').children().eq(0).children().eq(1)
+      image1Container.replaceWith(image1)
+
+      let temp1 = (data.list[5].main.temp)
+      var containerOne = document.querySelector('#temp1')
+      containerOne.innerHTML = 'Temp: _ ' + temp1 + '\u00B0'
+
+      let wind1 = (data.list[5].wind.speed)
+      var windContainerOne = document.querySelector('#wind1')
+      windContainerOne.innerHTML = 'Wind: _ ' + wind1 + ' MPH'
+
+      let humidity1 = (data.list[5].main.humidity)
+      var humidityContainerOne = document.querySelector('#humidity1')
+      humidityContainerOne.innerHTML = 'Humidity: _ ' + humidity1 + ' %'
+
+      let date2 = (data.list[13].dt_txt)
+      $('#date2').text(dayjs(date2).format('M/DD/YYYY'))
+
+      let forcast2Icon = (data.list[13].weather[0].icon)
+      window.image2 = document.createElement('img')
+      image2.src = ('http://openweathermap.org/img/wn/' + forcast2Icon + '.png')
+      image2.widt_txth = 80
+      image2.height = 90
+      let image2Container = $('#forecast').children().eq(2).children().eq(1)
+      image2Container.replaceWith(image2)
+
+      console.log(image2)
+
+      let temp2 = (data.list[13].main.temp)
+      var containerTwo = document.querySelector('#temp2')
+      containerTwo.innerHTML = 'Temp: _ ' + temp2 + '\u00B0'
+
+      let wind2 = (data.list[13].wind.speed)
+      var windContainerTwo = document.querySelector('#wind2')
+      windContainerTwo.innerHTML = 'Wind: _ ' + wind2 + ' MPH'
+
+      let humidity2 = (data.list[13].main.humidity)
+      var humidityContainerTwo = document.querySelector('#humidity2')
+      humidityContainerTwo.innerHTML = 'Humidity: _ ' + humidity2 + ' %'
+
+      let date3 = (data.list[21].dt_txt)
+      $('#date3').text(dayjs(date3).format('M/DD/YYYY'))
+
+      let forcast3Icon = (data.list[21].weather[0].icon)
+      window.image3 = document.createElement('img')
+      image3.src = ('http://openweathermap.org/img/wn/' + forcast3Icon + '.png')
+      image3.widt_txth = 80
+      image3.height = 90
+      let image3Container = $('#forecast').children().eq(4).children().eq(1)
+      image3Container.replaceWith(image3)
+
+      let temp3 = (data.list[21].main.temp)
+      var containerThree = document.querySelector('#temp3')
+      containerThree.innerHTML = 'Temp: _ ' + temp3 + '\u00B0'
+
+      let wind3 = (data.list[21].wind.speed)
+      var windContainerThree = document.querySelector('#wind3')
+      windContainerThree.innerHTML = 'Wind: _ ' + wind3 + ' MPH'
+
+      let humidity3 = (data.list[21].main.humidity)
+      var humidityContainerThree = document.querySelector('#humidity3')
+      humidityContainerThree.innerHTML = 'Humidity: _ ' + humidity3 + ' %'
+
+      let date4 = (data.list[29].dt_txt)
+      $('#date4').text(dayjs(date4).format('M/DD/YYYY'))
+
+      let forcast4Icon = (data.list[29].weather[0].icon)
+      window.image4 = document.createElement('img')
+      image4.src = ('http://openweathermap.org/img/wn/' + forcast4Icon + '.png')
+      image4.widt_txth = 80
+      image4.height = 90
+      let image4Container = $('#forecast').children().eq(6).children().eq(1)
+      image4Container.replaceWith(image4)
+
+      console.log(window.image4)
+
+      let temp4 = (data.list[29].main.temp)
+      var containerFour = document.querySelector('#temp4')
+      containerFour.innerHTML = 'Temp: _ ' + temp4 + '\u00B0'
+
+      let wind4 = (data.list[29].wind.speed)
+      var windContainerFour = document.querySelector('#wind4')
+      windContainerFour.innerHTML = 'Wind: _ ' + wind4 + ' MPH'
+
+      let humidity4 = (data.list[29].main.humidity)
+      var humidityContainerFour = document.querySelector('#humidity4')
+      humidityContainerFour.innerHTML = 'Humidity: _ ' + humidity4 + ' %'
+
+      let date5 = (data.list[37].dt_txt)
+      $('#date5').text(dayjs(date5).format('M/DD/YYYY'))
+
+      let forcast5Icon = (data.list[37].weather[0].icon)
+      window.image5 = document.createElement('img')
+      image5.src = ('http://openweathermap.org/img/wn/' + forcast5Icon + '.png')
+      image5.widt_txth = 80
+      image5.height = 90
+      let image5Container = $('#forecast').children().eq(8).children().eq(1)
+      image5Container.replaceWith(image5)
+
+      let temp5 = (data.list[37].main.temp)
+      var containerFive = document.querySelector('#temp5')
+      containerFive.innerHTML = 'Temp: _ ' + temp5 + '\u00B0'
+
+      let wind5 = (data.list[37].wind.speed)
+      var windContainerFive = document.querySelector('#wind5')
+      windContainerFive.innerHTML = 'Wind: _ ' + wind5 + ' MPH'
+
+      let humidity5 = (data.list[37].main.humidity)
+      var humidityContainerFive = document.querySelector('#humidity5')
+      humidityContainerFive.innerHTML = 'Humidity: _ ' + humidity5 + ' %'
+    })
+}
+
+function loadStorage () {
+  var history = JSON.parse(localStorage.getItem('cityList'))
+  console.log(history)
+  if (history == null) {
+    localStorage.setItem('cityList', JSON.stringify([]))
+    return
+  }
+  for (i = 0; i < history.length; i++) {
+    cityName = document.createElement('li')
+    cityName.innerHTML = history[i]
+    cityList.appendChild(cityName)
+    cityName.addEventListener('click' , storage)
+  }
+}
+
+loadStorage()
